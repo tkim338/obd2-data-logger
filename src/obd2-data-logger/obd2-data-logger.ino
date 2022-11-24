197f628b76d40527aa1f9a35f895c9821265f28a
Thomas Kim <tjk103@gmail.com>
2022-11-23 23:28:33 -0500

    wip

--- src/obd2-data-logger/obd2-data-logger.ino
+++ src/obd2-data-logger/obd2-data-logger.ino
@@ -1,66 +1,70 @@
-#include <SPI.h>
+//#include <SPI.h>
 #include <SD.h>
-#include <SoftwareSerial.h>
-#include <Canbus.h>
+//#include <SoftwareSerial.h>
+#include <CAN.h>
+#include <OBD2.h>
 
 // Chip Select pin is tied to pin 9 on the SparkFun CAN-Bus Shield
 const int chipSelect = 9;
 
-// Define Joystick connection pins 
+// Define Joystick connection pins
 //#define UP     A1
 //#define DOWN   A3
 //#define LEFT   A2
 //#define RIGHT  A5
 #define CLICK  A4
 
-//Define LED pins
+// Define LED pins
 #define LED_A 7
 #define LED_B 8
 
 // Declare SD File
 File dataFile;
 
-// Declare CAN variables for communication
-char *EngineRPM, *VehicleSpeed, *ThrottlePosition;
-
-// Data will be temporarily stored to this buffer before being written to the file
-char engineRpmBuffer[64], vehicleSpeedBuffer[64], throttlePositionBuffer[64];
-
 bool isActive; // State of logging activity
 
 // Names of files to write config info and data to
-char configFilename[] = "config.txt";
-char dataFilename[] = "data.csv";
-char columnHeaders[] = "engine_rpm,vehicle_speed,throttle_position";
+String configFilename = "config.txt";
+String dataFilename = "data.csv";
+String columnHeaders = "";
+int pidsOfInterest[] = {
+  ::CALCULATED_ENGINE_LOAD,
+  ::ENGINE_COOLANT_TEMPERATURE,
+  ::INTAKE_MANIFOLD_ABSOLUTE_PRESSURE,
+  ::ENGINE_RPM,
+  ::VEHICLE_SPEED,
+  ::TIMING_ADVANCE,
+  ::THROTTLE_POSITION
+};
 
 //********************************Setup Loop*********************************//
 void setup() {
   // Initialize Serial communication for debugging
   Serial.begin(9600);
   Serial.println("*** ECU data logger initializing ***");
-  
+
   // Initialize pins as necessary
   pinMode(chipSelect, OUTPUT);
   pinMode(CLICK, INPUT);
   pinMode(LED_A, OUTPUT);
   pinMode(LED_B, OUTPUT);
-  
+
   // Pull analog pins high to enable reading of joystick movements
   digitalWrite(CLICK, HIGH);
-  
+
   // Write LED pins low to turn them on
   digitalWrite(LED_A, HIGH);
   digitalWrite(LED_B, HIGH);
-  
-  // Initialize CAN Controller 
-  if (Canbus.init(CANSPEED_500)) {  // Initialize MCP2515 CAN controller at the specified speed
+
+  // Initialize CAN Controller
+  if (OBD2.begin()) {
     Serial.println("CAN init successful");
-    delay(1500);
-  } 
+    delay(100);
+  }
   else {
     Serial.println("CAN init failed");
     return;
-  } 
+  }
 
   // Check if uSD card initialized
   if (!SD.begin(chipSelect)) {
@@ -69,12 +73,12 @@ void setup() {
   }
   else {
     Serial.println("uSD card initialized");
-    delay(1500);
+    delay(100);
   }
 
   // Check if data file exists.  If not, write column headers.
-  setupDataFile(dataFilename, columnHeaders);
   generateConfigFile(configFilename);
+  setupDataFile(dataFilename, columnHeaders);
 
   // Set initial state of logger
   isActive = false;
@@ -92,89 +96,74 @@ void loop() {
   else {
     Serial.println("Idle. Click to start logging.");
   }
-  
+
   if (isActive) {
-    digitalWrite(LED_A, HIGH); // Turn on LED_A to indicate active CAN Bus traffic
-    
-    Canbus.ecu_req(ENGINE_RPM, engineRpmBuffer); // Request engine RPM
-    EngineRPM = engineRpmBuffer;
-    Serial.print("Engine RPM: ");
-    Serial.println(EngineRPM);
-    delay(10);
-    
-    Canbus.ecu_req(VEHICLE_SPEED, vehicleSpeedBuffer); // Request vehicle speed
-    VehicleSpeed = vehicleSpeedBuffer;
-    Serial.print("Vehicle speed: ");
-    Serial.println(VehicleSpeed);
-    delay(10);
-
-    Canbus.ecu_req(THROTTLE, throttlePositionBuffer); // Request throttle position
-    ThrottlePosition = throttlePositionBuffer;
-    Serial.print("Throttle position: ");
-    Serial.println(ThrottlePosition);
-    delay(10);
-   
-    digitalWrite(LED_A, LOW); // Turn off LED_A
-    digitalWrite(LED_B, HIGH); // Turn on LED_B
-    delay(50);
-      
-    File dataFile = openFile(dataFilename);
-
-    dataFile.print(EngineRPM);
-    dataFile.print(",");
-    dataFile.print(VehicleSpeed);
-    dataFile.print(",");
-    dataFile.print(ThrottlePosition);
-    dataFile.println();
-
-    dataFile.flush();
-    dataFile.close(); // Close data logging file
-
-    digitalWrite(LED_B, LOW); // Turn off LED_B
+    String data = readEcuParams(pidsOfInterest);
+    String dataList[] = {data};
+    writeToFile(dataFilename, dataList, 1);
   }
-  
+
   // Check for joystick click and update state.
-  if (digitalRead(CLICK)==LOW) {
+  if (digitalRead(CLICK) == LOW) {
     isActive = !isActive;
-    while (digitalRead(CLICK)==LOW) {} // Hold code in this state until button is released
+    while (digitalRead(CLICK) == LOW) {} // Hold code in this state until button is released
   }
 }
 
 // Check if data file exists.  If not, write column headers to data file.
-void setupDataFile(char filename[], char columnHeaders[]) {
+void setupDataFile(String filename, String columnHeaders) {
   if (!SD.exists(filename)) {
     File dataFile = openFile(filename);
 
     dataFile.println(columnHeaders);
-    
+
     dataFile.flush();
     dataFile.close(); // Close data logging file
   }
 }
 
 // Create config file with available PIDs.
-void generateConfigFile(char filename[]) {
-  char *AvailablePidBuffer;
-  Canbus.ecu_req(0x00, AvailablePidBuffer); // Request all available PIDs
-  Serial.print("Available PIDs: ");
-  Serial.println(AvailablePidBuffer);
-
-  File configFile = openFile(filename);
-  configFile.print("Available PIDs: ");
-  configFile.println(AvailablePidBuffer);
-  
-  configFile.flush();
-  configFile.close();
+String generateConfigFile(String filename) {
+  String pids = "";
+  String pidNames = "";
+  for (int pid = 0; pid < 96; pid++) {
+    if (OBD2.pidSupported(pid)) {
+      pids = pids + (String) pid + ",";
+      pidNames = pidNames + OBD2.pidName(pid) + " [" + OBD2.pidUnits(pid) + "],";
+    }
+  }
+  String dataList[] = {pids, pidNames, ""};
+  writeToFile(filename, dataList, 3);
 }
 
-File openFile(char filename[]) {
+String readEcuParams(int pids[]) {
+  digitalWrite(LED_A, HIGH);
+  digitalWrite(LED_A, LOW);
+  return "";
+}
+
+File openFile(String filename) {
   File file = SD.open(filename, FILE_WRITE); // Open uSD file to log data
-  
+
   // If data file can't be opened, throw error.
-  if (!file){
+  if (!file) {
     Serial.println("Error opening file: ");
     Serial.println(filename);
   }
 
   return file;
+}
+
+void writeToFile(String filename, String lines[], int numLines) {
+  digitalWrite(LED_B, HIGH); // Turn on LED_B
+
+  File file = openFile(filename);
+  for (int i = 0; i < numLines; i++) {
+    file.print(lines[i]);
+    file.println();
+  }
+  file.flush();
+  file.close();
+
+  digitalWrite(LED_B, LOW); // Turn off LED_B
 }
